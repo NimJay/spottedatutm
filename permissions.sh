e5d4a1aa2b5bbdafacccf725d14fbfdacafedece
jayawar9 <nim.jayawardena@mail.utoronto.ca>
2016-05-08 14:36:31 -0400

    Create shell script for easy permission setting

---
+++ permissions.sh
@@ -0,0 +1,13 @@
+# Directories
+chmod 701 .
+chmod 701 js
+chmod 701 css
+chmod 701 images
+
+# Files
+chmod 704 images/*.png
+chmod 704 js/*.js
+chmod 704 js/lib/*.js
+chmod 704 css/*.css
+chmod 704 *.html
+
