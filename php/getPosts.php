0bab9a58a17045396380f10eb7bf30cf3a9293f7
jayawar9 <nim.jayawardena@mail.utoronto.ca>
2016-05-08 22:17:18 -0400

    Implement Post retrieval at backend

---
+++ php/getPosts.php
@@ -0,0 +1,74 @@
+<?php
+
+// Error reporting ON.
+ini_set('display_errors', 1);
+error_reporting(~0);
+
+$MAX_ID = 1000;		// Highest post id in database.
+
+// Categories.
+$FIRE = 0;
+$NEW = 1;
+
+// error_reporting(0);
+
+
+/* --------------- G L O B A L S -------------- */
+
+
+$stream = $_GET['stream']; // FIRE, NEW, etc.
+$x = $_GET['x']; // Number of posts to retrieve.
+// $lastId is NULL on the very first query.
+if ($_GET['lastId'] != NULL && is_numeric($_GET['lastId'])) {
+	$lastId = $_GET['lastId'];
+} else {
+	$lastId = $MAX_ID;
+}
+
+$output = array( // To be output as JSON.
+	"error" => false,
+	"posts" => NULL);
+
+if ($stream == NULL || $x == NULL) {
+	$output["error"] = true;
+	echo json_encode($output);
+	return;
+}
+
+/* ----- P R E P A R E   &   E X E C U T E ---- */
+
+$sql = new SQLite3("spottedatutm.db");
+$stmt = $sql->prepare("SELECT * FROM posts WHERE id < :lastId ORDER BY id DESC LIMIT :x");
+$stmt->bindValue(":lastId", $lastId);
+$stmt->bindValue(":x", $x);
+$result = $stmt->execute();
+
+if ($result) {
+
+	// Construct an array for each post.
+	$posts = array();
+	while ($row = $result->fetchArray()) {
+		$posts[] = array(
+			"id" => $row['id'],
+			"post" => htmlspecialchars($row['post']),
+			"author" => htmlspecialchars($row['author']),
+			"time" => htmlspecialchars($row['time']),
+			"likes" => $row['likes'],
+			"flags" => $row['flags']);
+	}
+	$output["posts"] = $posts;
+
+} else {
+	$output["error"] = true;
+}
+
+
+/* ----------------- C L O S E ---------------- */ 
+
+$result->finalize();
+$stmt->close();
+$sql->close();
+
+echo json_encode($output);
+
+?>
