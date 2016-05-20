d7847a0eb0ec48bb04d55a6a89e85ebb81fab5d7
jayawar9 <nim.jayawardena@mail.utoronto.ca>
2016-05-20 18:25:22 -0400

    Update shell script for setting permissions

--- permissions.sh
+++ permissions.sh
@@ -1,13 +1,20 @@
-# Directories
 chmod 701 .
-chmod 701 js
-chmod 701 css
-chmod 701 images
+chmod 704 index.html
+chmod 704 favicon.ico
 
-# Files
+# CSS
+chmod 704 css/index.css
+chmod 701 css
+
+# Images
+chmod 701 images
 chmod 704 images/*.png
+
+# JavaScript
+chmod 701 js
+chmod 701 js/lib
 chmod 704 js/*.js
 chmod 704 js/lib/*.js
-chmod 704 css/*.css
-chmod 704 *.html
 
+# PHP
+chmod 701 php
