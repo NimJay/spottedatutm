eea2af4954f303980409734e381ec1a9ef91b01d
jayawar9 <nim.jayawardena@mail.utoronto.ca>
2016-05-20 19:09:56 -0400

    Require authorization for schema.php

--- php/schema.php
+++ php/schema.php
@@ -1,5 +1,11 @@
 <?php
 
+// Authorization.
+if ($_GET['password'] != "framenumber") {
+	echo "Unauthorized.";
+	return;
+}
+
 // Error reporting ON.
 ini_set('display_errors', 1);
 error_reporting(~0);
@@ -8,22 +14,20 @@ error_reporting(~0);
 $db = new SQLite3('spottedatutm.db');
 
 // Create table for Posts.
-/*
+
 $db->exec("DROP TABLE posts;");
 $db->exec("CREATE TABLE posts (id INTEGER PRIMARY KEY AUTOINCREMENT, post VARCHAR(500) NOT NULL, author VARCHAR(100), time TIMESTAMP DEFAULT CURRENT_TIMESTAMP, likes UNSIGNED BIG INT DEFAULT 0, flags INTEGER DEFAULT 0, ip VARCHAR(15));");
-*/
+
 
 // Insert the initial posts.
-/*
+
 $db->exec("DELETE FROM posts WHERE 1=1");
-$db->exec("INSERT INTO posts (post, author) VALUES (\"The first post is born.\", \"Nim\");");
-$db->exec("INSERT INTO posts (post, author) VALUES (\"The second post is born.\", \"Nim\");");
-$db->exec("INSERT INTO posts (post, author) VALUES (\"The third post is born.\", \"Nim\");");
-$db->exec("INSERT INTO posts (post, author) VALUES (\"The fourth post is born.\", \"Nim\");");
-$db->exec("INSERT INTO posts (post, author) VALUES (\"The fifth post is born.\", \"Nim\");");
-$db->exec("INSERT INTO posts (post, author) VALUES (\"aNyOnE kNoW hOw To FiX a KeYbOaRd?\", \"ZAYN\");");
-$db->exec("INSERT INTO posts (post, author) VALUES (\"I got guns in my head and they won't go.\", \"Cage The Elephants\");");
-*/
+$db->exec("INSERT INTO posts (post, author, time) VALUES (\"The first post is born.\", \"Nim\", \"2016-05-14 19:42:51\");");
+$db->exec("INSERT INTO posts (post, author, time) VALUES (\"The second post is born.\", \"Nim\", \"2016-05-14 19:42:51\");");
+$db->exec("INSERT INTO posts (post, time) VALUES (\"To the guy in MGM101 who actually stood up to greet people, you the real MVP.\", \"2016-05-18 15:59:57\");");
+$db->exec("INSERT INTO posts (post, author, time) VALUES (\"Shout-outs to the second-floor janitors at DH for keeping it real.\", \"CSC Student\", \"2016-05-18 16:02:11\");");
+$db->exec("INSERT INTO posts (post, author, time) VALUES (\"Shout-out to the deer I saw yesterday.\", \"Fourth Year\", \"2016-05-19 16:16:18\");");
+
 
 
 // Display posts.
@@ -42,11 +46,11 @@ while ($row = $results->fetchArray()) {
 echo "</table>";
 
 // Create tables for Likes and Flags.
-/*
+
 $db->exec("DROP TABLE likes;");
 $db->exec("CREATE TABLE likes (id INTEGER NOT NULL, time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, ip VARCHAR(15) NOT NULL, PRIMARY KEY(id, ip), FOREIGN KEY (id) REFERENCES posts(id));");
 $db->exec("DROP TABLE flags;");
 $db->exec("CREATE TABLE flags (id INTEGER NOT NULL, time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, ip VARCHAR(15) NOT NULL, PRIMARY KEY(id, ip), FOREIGN KEY (id) REFERENCES posts(id));");
-*/
+
 
 ?>
