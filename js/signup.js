676c2672625cfa4d615b65991293551eb7b83a81
jayawar9 <nim.jayawardena@mail.utoronto.ca>
2016-05-23 12:29:57 -0400

    Animate deer image on Sign Up page

---
+++ js/signup.js
@@ -0,0 +1,8 @@
+/*-------------------------------- S I G N U P -------------------------------*/
+
+
+$(function () {
+	
+	$("img").animate({left:"0px"}, 1000); // Slide deer in.
+	$("#signup-email").focus();
+});
